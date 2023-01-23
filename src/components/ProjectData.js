ee3c3967d4df30f472c5f834c0a76ffe048eac6a
Tunde philps <90014837+tundephilps@users.noreply.github.com>
2023-01-23 07:42:26 +0100

    Add files via upload

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,43 +1,40 @@
 const ProjectData = [
+  {
+    id: "1",
+    date: "July 10 2021",
+    image: "../images/page1.png",
+    title: "CryptoCurrency Tracker",
+    Link: "https://optimum-crypto.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Optimum-Crypto",
+    description:
+      "A CryptoCurrency Tracking App that shows live data of all Current Market Prices of trending Crypto coins, all built from the scratch and fully responsive for mobile devices.",
+  },
+  {
+    id: "2",
+    date: "January 17 2022",
+    image: "../images/page7.png",
+    title: "Movie WebApp",
+    Link: "https://optimum-cinema.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/CInema-App",
+    description:
+      "A Cinema Trailer and Movie Review WebApp, built with complex Algorithms and Real Time movie data of the latest Hollywood Movies and TV series",
+  },
 
-    {
-        id: "1",
-        date: "July 10 2021",
-        image: "../images/page1.png",
-        title: "CryptoCurrency Tracker",
-        Link: "https://optimum-crypto.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Optimum-Crypto",
-        description: "A CryptoCurrency Tracking App that shows live data of all Current Market Prices of trending Crypto coins, all built from the scratch and fully responsive for mobile devices.",
+  {
+    id: "3",
+    date: "January 17 2022",
+    image: "../images/MusicPlayer.png",
+    title: "Redux Music Player",
+    Link: "https://music-player-gamma-blue.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Redux-Music-Player",
+    description:
+      "A Music Streaming App Built with Redux, Rapid Api extension and the Shazam Core API for streaming music data and of course tailwind css was used for styling...",
+  },
 
-    },
-    {
-     
-        id: "2",
-        date: "January 17 2022",
-        image: "../images/page7.png",
-        title: "Movie WebApp",
-        Link: "https://optimum-cinema.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/CInema-App",
-        description: "A Cinema Trailer and Movie Review WebApp, built with complex Algorithms and Real Time movie data of the latest Hollywood Movies and TV series",
-
-    },
-    
-
-    {
-     
-        id: "3",
-        date: "January 17 2022",
-        image: "../images/MusicPlayer.png",
-        title: "Redux Music Player",
-        Link: "https://music-player-gamma-blue.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Redux-Music-Player",
-        description: "A Music Streaming App Built with Redux, Rapid Api extension and the Shazam Core API for streaming music data and of course tailwind css was used for styling..."
-    },
-
-{    
+  {
     id: "4",
     date: "March 28 2021",
     image: "../images/page8.png",
@@ -45,133 +42,127 @@ const ProjectData = [
     Link: "https://university-brown.vercel.app/",
     tech: "HTML, CSS, Javascript, React, Redux Toolkit",
     sourcecode: "https://github.com/tundephilps/Educational-WebApp",
-    description: "A university web app that includes a Students Forum, Price and Checkout Page for payment of tution, a staff management software that edits and deletes records of staffs, a Quiz App., Accoridion for FAQ and Login page... All in one.",
+    description:
+      "A university web app that includes a Students Forum, Price and Checkout Page for payment of tution, a staff management software that edits and deletes records of staffs, a Quiz App., Accoridion for FAQ and Login page... All in one.",
+  },
 
-    
-        
-    },
+  {
+    id: "5",
+    date: "March 7 2021",
+    image: "../images/Blog.png",
+    title: "Blog Project",
+    Link: "https://blog-tundephilps.vercel.app/",
+    tech: "HTML, CSS, Javascript, Axios, React, Sanity,io",
+    sourcecode: "https://github.com/tundephilps/Blog",
+    description:
+      "A Blog App integrated with Graph QL/ Apollo Client and a backend service called Sanity.io for posting articles in real time, a news app with data fetched from a news api which unfortunatly doesnt display in production because its a free version but displays fully well on localhost, still at the building phase ... Still work in progress Visit ",
+  },
 
-    
-    {
-        id: "5",
-        date: "March 7 2021",
-        image: "../images/Blog.png",
-        title: "Blog Project",
-        Link: "https://blog-tundephilps.vercel.app/",
-        tech: "HTML, CSS, Javascript, Axios, React, Sanity,io",
-        sourcecode: "https://github.com/tundephilps/Blog",
-        description: "A Blog App integrated with Graph QL/ Apollo Client and a backend service called Sanity.io for posting articles in real time, a news app with data fetched from a news api which unfortunatly doesnt display in production because its a free version but displays fully well on localhost, still at the building phase ... Still work in progress Visit ",
-    
-        
-        },
-        
-    {
-        id: "6",
-        date: "Sept 23 2022",
-        image: "../images/Chat.png",
-        title: "Chat App",
-        Link: "https://chat-app-xi-flame.vercel.app",
-        tech: "HTML, SCSS, Javascript, React, FireBase",
-        sourcecode: "https://github.com/tundephilps/Chat-App",
-        description: "A Full stack Chat App built with React and Firebase... With Authentication where multiple users can chat and interact with each other...done mainly for tutorial purpose to develop my backend Skills with Firebase",
-        },
-    
-    
-    
-    {
-        id: "7",
-        date: "Oct 13 2022",
-        image: "../images/CoreReact.png",
-        title: "Core React Projects",
-        Link: "https://core-react-tundephilps.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Core-React-Projects",
-        description: "A collecton of Core React Projects and concepts i used in sharpening my React Skills over time which includes a Password Strenght meter, Login Form, Qr Code Generator, BookList App etc....",
-        
-      
-        
-    },
-     
-    {
-        id: "8",
-        date: "Oct 27 2022",
-        image: "../images/Redux.png",
-        title: "Redux Collection",
-        Link: "https://redux-projects-tundephilps.vercel.app/",
-        tech: "HTML,Tailwind CSS, Javascript, React-Redux",
-        sourcecode: "https://github.com/tundephilps/Redux-Projects",
-        description: "A collection of Redux projects i have done over time, both for Personal Projects and turtorial purposes. Still Work in Progress...",
-        
-      
-        
-    },
-   
+  {
+    id: "6",
+    date: "Sept 23 2022",
+    image: "../images/Chat.png",
+    title: "Chat App",
+    Link: "https://chat-app-xi-flame.vercel.app",
+    tech: "HTML, SCSS, Javascript, React, FireBase",
+    sourcecode: "https://github.com/tundephilps/Chat-App",
+    description:
+      "A Full stack Chat App built with React and Firebase... With Authentication where multiple users can chat and interact with each other...done mainly for tutorial purpose to develop my backend Skills with Firebase",
+  },
 
-    
-    {
-        id: "9",
-        date: "Sept 23 2021",
-        image: "../images/page3.png",
-        title: "Hotel Website",
-        Link: "https://hotel-kohl.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Hotel-WebApp",
-        description: "A project made for a 5star hotel to uplift the design of their Business in the shortest time span.   The code is well implemented and a pleasure to work with the staffs  amazes me with the creativity and variations introduced. I not only make them look professional but also Advertise there services to the fullest ",
-    
-      
-        
-    },
+  {
+    id: "7",
+    date: "Jan 11 2023",
+    image: "../images/Softcollection.png",
+    title: "Software UI Collection",
+    Link: "https://software-ui-tundephilps.vercel.app",
+    tech: "HTML, SCSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Software-Ui-Collection",
+    description:
+      "A Collection of Users Interface for diffrent Range of Sotwares",
+  },
 
-    {
-        id: "10",
-        date: "Aug 21 2022",
-        image: "../images/arcade.png",
-        title: "Game Store & Arcade",
-        Link: "https://game-store-tundephilps.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Game-Store-and-Arcade",
-        description: "A Video game store and Arcade that includes an Ecommerce page for selling game products and items and a game arcade that users can play games like tetris on the web app, all these built with react. Still work in progress 30% complete. ",
-    
-          
-    },
-    {
-        id: "11",
-        date: "Feb 23 2022",
-        image: "../images/collection.png",
-        title: "Landing Page Collection",
-        Link: "https://landing-pages-collection-bay.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Landing-Pages-Collection",
-        description: "A Collection of Landing Pages i made over time for personal projects and freelance gigs...",
-    
-          
-    },
+  {
+    id: "8",
+    date: "Oct 13 2022",
+    image: "../images/CoreReact.png",
+    title: "Core React Projects",
+    Link: "https://core-react-tundephilps.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Core-React-Projects",
+    description:
+      "A collecton of Core React Projects and concepts i used in sharpening my React Skills over time which includes a Password Strenght meter, Login Form, Qr Code Generator, BookList App etc....",
+  },
 
-    {
-        id: "12",
-        date: "Dec 8 2022",
-        image: "../images/Portal.png",
-        title: "Job Portal",
-        Link: "https://job-portal-beta.vercel.app/",
-        tech: "HTML, CSS, Material UI, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Job-Portal",
-        description: "A Job Portal App which displays a list of jobs avaliable and classifed into an array of data and each job posting has its own unique url for application...",
-    
-          
-    },
-    {
-     
-        id: "13",
-        date: "December 8 2022",
-        image: "../images/Ricky.png",
-        title: "Ricky and Morty App",
-        Link: "https://ricky-gules.vercel.app/",
-        tech: "HTML, CSS, Javascript, React",
-        sourcecode: "https://github.com/tundephilps/Ricky-and-Morty-App",
-        description: "An App that makes an API Call and includes a search, filter and pagination"
-    },
+  {
+    id: "9",
+    date: "Oct 27 2022",
+    image: "../images/Redux.png",
+    title: "Redux Collection",
+    Link: "https://redux-projects-tundephilps.vercel.app/",
+    tech: "HTML,Tailwind CSS, Javascript, React-Redux",
+    sourcecode: "https://github.com/tundephilps/Redux-Projects",
+    description:
+      "A collection of Redux projects i have done over time, both for Personal Projects and turtorial purposes. Still Work in Progress...",
+  },
 
-   
-    ]
-      
-    export default ProjectData;
+  {
+    id: "10",
+    date: "Sept 23 2021",
+    image: "../images/page3.png",
+    title: "Hotel Website",
+    Link: "https://hotel-kohl.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Hotel-WebApp",
+    description:
+      "A project made for a 5star hotel to uplift the design of their Business in the shortest time span.   The code is well implemented and a pleasure to work with the staffs  amazes me with the creativity and variations introduced. I not only make them look professional but also Advertise there services to the fullest ",
+  },
+
+  {
+    id: "11",
+    date: "Aug 21 2022",
+    image: "../images/arcade.png",
+    title: "Game Store & Arcade",
+    Link: "https://game-store-tundephilps.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Game-Store-and-Arcade",
+    description:
+      "A Video game store and Arcade that includes an Ecommerce page for selling game products and items and a game arcade that users can play games like tetris on the web app, all these built with react. Still work in progress 30% complete. ",
+  },
+  {
+    id: "12",
+    date: "Feb 23 2022",
+    image: "../images/collection.png",
+    title: "Landing Page Collection",
+    Link: "https://landing-pages-collection-bay.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Landing-Pages-Collection",
+    description:
+      "A Collection of Landing Pages i made over time for personal projects and freelance gigs...",
+  },
+
+  {
+    id: "13",
+    date: "Dec 8 2022",
+    image: "../images/Portal.png",
+    title: "Job Portal",
+    Link: "https://job-portal-beta.vercel.app/",
+    tech: "HTML, CSS, Material UI, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Job-Portal",
+    description:
+      "A Job Portal App which displays a list of jobs avaliable and classifed into an array of data and each job posting has its own unique url for application...",
+  },
+  {
+    id: "14",
+    date: "December 8 2022",
+    image: "../images/Ricky.png",
+    title: "Ricky and Morty App",
+    Link: "https://ricky-gules.vercel.app/",
+    tech: "HTML, CSS, Javascript, React",
+    sourcecode: "https://github.com/tundephilps/Ricky-and-Morty-App",
+    description:
+      "An App that makes an API Call and includes a search, filter and pagination",
+  },
+];
+
+export default ProjectData;
